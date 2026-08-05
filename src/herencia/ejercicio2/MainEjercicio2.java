2e3d7028573d4a05b31bf46586dd28abf2ff68af
Brandonpyjv <brandonar1997@gmail.com>
2026-08-05 11:46:51 -0500

    Crear clase AprendizSena y modificar Main

--- src/herencia/ejercicio2/MainEjercicio2.java
+++ src/herencia/ejercicio2/MainEjercicio2.java
@@ -4,8 +4,11 @@ public class MainEjercicio2 {
     public static void main(String[] args) {
         Vendedor v = new Vendedor("Ana Lopez", "1234", 1500000, 350000);
         Gerente g = new Gerente("Pedro Ruiz", "5678", 3000000, 800000);
+        AprendizSena a = new AprendizSena("Juan Perez", "9012", 1160000);
         v.mostrarInfo();
         System.out.println("---");
         g.mostrarInfo();
+        System.out.println("---");
+        a.mostrarInfo();
     }
 }
