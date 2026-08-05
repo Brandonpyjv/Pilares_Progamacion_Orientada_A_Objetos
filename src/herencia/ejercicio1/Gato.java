4008b8f166fd52ff835155292534ecd58bc205b8
Brandonpyjv <brandonar1997@gmail.com>
2026-08-05 09:51:29 -0500

    crear clase gato y metodos

--- src/herencia/ejercicio1/Gato.java
+++ src/herencia/ejercicio1/Gato.java
@@ -1,4 +1,18 @@
 package herencia.ejercicio1;
 
-public class Gato {
+
+public class Gato extends Animal {
+
+    public Gato(String nombre, int edad) {
+        super(nombre, edad);
+    }
+
+    @Override
+    public void hacerSonido() {
+        System.out.println(nombre + " hace Miau Miau");
+    }
+
+    public void ronronear() {
+        System.out.println(nombre + " Pruuuuu");
+    }
 }
