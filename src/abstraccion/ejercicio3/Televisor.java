8eb6e84c6d05e4689e456e5ecce8ddb576197ff0
Brandonpyjv <brandonar1997@gmail.com>
2026-08-06 10:12:56 -0500

    Create Televisor class

---
+++ src/abstraccion/ejercicio3/Televisor.java
@@ -0,0 +1,15 @@
+package abstraccion.ejercicio3;
+
+public class Televisor extends Electrodomestico {
+    public Televisor(String marca) {
+        super(marca);
+    }
+    @Override
+    public void funcionPrincipal() {
+        if (encendido) {
+            System.out.println("Mostrando el canal en pantalla...");
+        } else {
+            System.out.println("Enciendalo primero.");
+        }
+    }
+}
