470d7320d86310d423d3617d4fc00c1f503307a6
Brandonpyjv <brandonar1997@gmail.com>
2026-08-06 10:29:41 -0500

    Create PagoEfectivo

---
+++ src/abstraccion/ejercicio4/PagoEfectivo.java
@@ -0,0 +1,13 @@
+package abstraccion.ejercicio4;
+
+public class PagoEfectivo extends MedioPago {
+    public PagoEfectivo(double monto) {
+        super(monto);
+    }
+    @Override
+    public void procesarPago() {
+        int codigo = (int)(Math.random() * 100000);
+        System.out.println("Genere el codigo de recaudo: " + codigo);
+        System.out.println("Acerquese a Efecty a pagar $" + monto);
+    }
+}
