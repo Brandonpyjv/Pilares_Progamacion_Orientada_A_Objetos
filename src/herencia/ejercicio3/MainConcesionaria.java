e3d046a41fbd15d6980aad57a1db436140d82a7f
Brandonpyjv <brandonar1997@gmail.com>
2026-08-07 01:06:59 -0500

    Create MainConcesionaria.java

---
+++ src/herencia/ejercicio3/MainConcesionaria.java
@@ -0,0 +1,11 @@
+package herencia.ejercicio3;
+
+public class MainConcesionaria {
+    public static void main(String[] args) {
+        Carro c = new Carro("Chevrolet", "Spark", 45000000, 4);
+        Moto m = new Moto("Yamaha", "FZ", 12000000, 250);
+        c.describir();
+        System.out.println("---");
+        m.describir();
+    }
+}
