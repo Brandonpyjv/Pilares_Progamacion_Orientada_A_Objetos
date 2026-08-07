376ec142d7fab09b6cb24d6f0fab9b9f84f872be
Brandonpyjv <brandonar1997@gmail.com>
2026-08-07 00:59:05 -0500

    Add NotificacionWhatsapp class and the main was modified to include that new feature

--- src/abstraccion/ejercicio5/MainNotificaciones.java
+++ src/abstraccion/ejercicio5/MainNotificaciones.java
@@ -8,8 +8,13 @@ public class MainNotificaciones {
         NotificacionSMS sms = new NotificacionSMS(
                 "3001234567",
                 "Su pedido esta en camino.");
+        NotificacionWhatsApp whatsapp = new NotificacionWhatsApp(
+                "3007654321",
+                "Su factura electronica ya esta disponible.");
         email.enviar();
         System.out.println("---");
         sms.enviar();
+        System.out.println("---");
+        whatsapp.enviar();
     }
 }
