03069c1e013157d7323cb3f4c447d52de233a21a
Brandonpyjv <brandonar1997@gmail.com>
2026-08-07 01:26:26 -0500

    create administrador class and main was modified

--- src/herencia/ejercicio5/MainPlataforma.java
+++ src/herencia/ejercicio5/MainPlataforma.java
@@ -6,8 +6,12 @@ public class MainPlataforma {
                 "carlos@sena.edu.co", 5);
         Instructor i = new Instructor("Marta Soto",
                 "marta@sena.edu.co", "Java");
+        Administrador a = new Administrador("Laura Diaz",
+                "laura@sena.edu.co", "TOTAL");
         e.iniciarSesion();
         System.out.println("---");
         i.iniciarSesion();
+        System.out.println("---");
+        a.iniciarSesion();
     }
 }
